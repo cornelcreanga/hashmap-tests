7762560911ec4f0ae2c533ebd82e33e1c1c2e136
cornel creanga <cornelcreanga@yahoo.com>
2016-07-16 18:23:42 +0300

    update dependencies (mostly)

--- src/main/java/com/cornel/hashtest/util/ObjectSize.java
+++ src/main/java/com/cornel/hashtest/util/ObjectSize.java
@@ -12,14 +12,16 @@ public class ObjectSize {
         MemoryMeter meter = new MemoryMeter();
 
         System.out.println("Object:" + meter.measureDeep(new Object()));
-        System.out.println("Integer:" + meter.measureDeep(new Integer(234321)));
-        System.out.println("Long:" + meter.measureDeep(new Long(54353l)));
-        System.out.println("Double:" + meter.measureDeep(new Double(54353l)));
+        System.out.println("Integer:" + meter.measureDeep(234321));
+        System.out.println("Long:" + meter.measureDeep(54353L));
+        System.out.println("Double:" + meter.measureDeep((double) 54353L));
         System.out.println("ArrayList:" + meter.measureDeep(new ArrayList()));
         System.out.println("HashMap:" + meter.measureDeep(new HashMap()));
-        System.out.println("16 elements byte array:" + meter.measureDeep(new byte[10]));
-        System.out.println("16 elements int array:" + meter.measureDeep(new int[10]));
-        System.out.println("16 elements Object array:" + meter.measureDeep(new Object[10]));
+        System.out.println("16 elements byte array:" + meter.measureDeep(new byte[16]));
+        System.out.println("16 elements char array:" + meter.measureDeep(new char[16]));
+        System.out.println("16 elements int array:" + meter.measureDeep(new int[16]));
+        System.out.println("16 elements Object array:" + meter.measureDeep(new Object[16]));
+
         System.out.println("4 char string:" + meter.measureDeep("tiny"));
         System.out.println("20 char string:" + meter.measureDeep("20 I'm a small string"));
         System.out.println("100 char string:" + meter.measureDeep("100 I'm a longer string la la la la la la la la la la la la la la la la la la la la la la la la la ."));
